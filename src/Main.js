2cea70497e929dda1d0085056353ea1902971515
woochoi <woochoi@student.42seoul.kr>
2023-09-15 14:08:25 +0900

    rename: Update directory name from 'src/component' to 'src/components'

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './component/App';
+import App from './components/App';
 import Home from './pages/Home';
 
 function Main() {
