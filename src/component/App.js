7dfed78bfbea34de3dd66eb740f66502a361f8a3
woochoi <woochoi@student.42seoul.kr>
2023-09-15 14:07:00 +0900

    feat: create nav component

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="body">
-      <Outlet />
-    </div>
+    <>
+      <Nav />
+      <div className="body">
+        <Outlet />
+      </div>
+    </>
   );
 }
 
---
+++ src/component/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="Logo">Universal Code</div>
+      <ul>
+        <li>Posts</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
